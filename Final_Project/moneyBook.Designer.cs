fdb543e5d8b54daf9f9a209ba09b781c19d520c1
Eva <p98286825@gmail.com>
2022-12-18 16:19:00 +0800

    add income/outcome button design

--- Final_Project/moneyBook.Designer.cs
+++ Final_Project/moneyBook.Designer.cs
@@ -28,61 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.AddBtn = new System.Windows.Forms.Button();
-            this.CheckBtn = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(moneyBook));
+            this.HomePanel = new System.Windows.Forms.Panel();
             this.backBtn = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.addPanel = new System.Windows.Forms.Panel();
+            this.button_icons = new System.Windows.Forms.ImageList(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.addBackBtn = new System.Windows.Forms.Button();
+            this.HomePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.addPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // HomePanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
-            this.panel1.Controls.Add(this.backBtn);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.CheckBtn);
-            this.panel1.Controls.Add(this.AddBtn);
-            this.panel1.Location = new System.Drawing.Point(0, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(802, 454);
-            this.panel1.TabIndex = 0;
-            // 
-            // AddBtn
-            // 
-            this.AddBtn.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddBtn.Location = new System.Drawing.Point(226, 201);
-            this.AddBtn.Name = "AddBtn";
-            this.AddBtn.Size = new System.Drawing.Size(346, 57);
-            this.AddBtn.TabIndex = 0;
-            this.AddBtn.Text = "Add income/outcome";
-            this.AddBtn.UseVisualStyleBackColor = true;
-            // 
-            // CheckBtn
-            // 
-            this.CheckBtn.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CheckBtn.Location = new System.Drawing.Point(226, 277);
-            this.CheckBtn.Name = "CheckBtn";
-            this.CheckBtn.Size = new System.Drawing.Size(346, 57);
-            this.CheckBtn.TabIndex = 1;
-            this.CheckBtn.Text = "Check stats";
-            this.CheckBtn.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Final_Project.Properties.Resources.pig;
-            this.pictureBox1.Location = new System.Drawing.Point(330, 35);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(137, 146);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.HomePanel.BackColor = System.Drawing.Color.PowderBlue;
+            this.HomePanel.Controls.Add(this.backBtn);
+            this.HomePanel.Controls.Add(this.pictureBox1);
+            this.HomePanel.Controls.Add(this.CheckBtn);
+            this.HomePanel.Controls.Add(this.AddBtn);
+            this.HomePanel.Location = new System.Drawing.Point(0, 1);
+            this.HomePanel.Name = "HomePanel";
+            this.HomePanel.Size = new System.Drawing.Size(986, 454);
+            this.HomePanel.TabIndex = 0;
             // 
             // backBtn
             // 
             this.backBtn.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backBtn.Location = new System.Drawing.Point(226, 354);
+            this.backBtn.Location = new System.Drawing.Point(302, 357);
             this.backBtn.Name = "backBtn";
             this.backBtn.Size = new System.Drawing.Size(346, 57);
             this.backBtn.TabIndex = 3;
@@ -90,27 +67,113 @@
             this.backBtn.UseVisualStyleBackColor = true;
             this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Final_Project.Properties.Resources.pig;
+            this.pictureBox1.Location = new System.Drawing.Point(412, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBtn.Location = new System.Drawing.Point(302, 280);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(346, 57);
+            this.CheckBtn.TabIndex = 1;
+            this.CheckBtn.Text = "Check stats";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddBtn.Location = new System.Drawing.Point(302, 204);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(346, 57);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.Text = "Add income/outcome";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // addPanel
+            // 
+            this.addPanel.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.addPanel.Controls.Add(this.addBackBtn);
+            this.addPanel.Location = new System.Drawing.Point(0, 1);
+            this.addPanel.Name = "addPanel";
+            this.addPanel.Size = new System.Drawing.Size(986, 453);
+            this.addPanel.TabIndex = 1;
+            // 
+            // button_icons
+            // 
+            this.button_icons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("button_icons.ImageStream")));
+            this.button_icons.TransparentColor = System.Drawing.Color.Transparent;
+            this.button_icons.Images.SetKeyName(0, "0.png");
+            this.button_icons.Images.SetKeyName(1, "1.png");
+            this.button_icons.Images.SetKeyName(2, "2.png");
+            this.button_icons.Images.SetKeyName(3, "3.png");
+            this.button_icons.Images.SetKeyName(4, "4.png");
+            this.button_icons.Images.SetKeyName(5, "5.png");
+            this.button_icons.Images.SetKeyName(6, "6.png");
+            this.button_icons.Images.SetKeyName(7, "7.png");
+            this.button_icons.Images.SetKeyName(8, "8.png");
+            this.button_icons.Images.SetKeyName(9, "9.png");
+            this.button_icons.Images.SetKeyName(10, "10.png");
+            this.button_icons.Images.SetKeyName(11, "11.png");
+            this.button_icons.Images.SetKeyName(12, "12.png");
+            this.button_icons.Images.SetKeyName(13, "13.png");
+            this.button_icons.Images.SetKeyName(14, "14.png");
+            // 
+            // addBackBtn
+            // 
+            this.addBackBtn.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.addBackBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.addBackBtn.FlatAppearance.BorderSize = 2;
+            this.addBackBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.BurlyWood;
+            this.addBackBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.BurlyWood;
+            this.addBackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addBackBtn.Font = new System.Drawing.Font("Ink Free", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBackBtn.ForeColor = System.Drawing.Color.DarkBlue;
+            this.addBackBtn.Location = new System.Drawing.Point(889, 390);
+            this.addBackBtn.Name = "addBackBtn";
+            this.addBackBtn.Size = new System.Drawing.Size(83, 47);
+            this.addBackBtn.TabIndex = 0;
+            this.addBackBtn.Text = "Back";
+            this.addBackBtn.UseVisualStyleBackColor = false;
+            this.addBackBtn.Click += new System.EventHandler(this.addBackBtn_Click);
+            // 
             // moneyBook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.addPanel);
+            this.Controls.Add(this.HomePanel);
             this.Name = "moneyBook";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MoneyBook";
-            this.panel1.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.moneyBook_Load);
+            this.HomePanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.addPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel HomePanel;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button CheckBtn;
         private System.Windows.Forms.Button AddBtn;
         private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Panel addPanel;
+        private System.Windows.Forms.ImageList button_icons;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button addBackBtn;
     }
 }
--- Final_Project/moneyBook.cs
+++ Final_Project/moneyBook.cs
@@ -12,6 +12,7 @@ namespace Final_Project
 {
     public partial class moneyBook : Form
     {
+        Button[] iconBtns;
         public moneyBook()
         {
             InitializeComponent();
@@ -20,6 +21,89 @@ namespace Final_Project
         private void backBtn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            addPanel.Visible = true;
+            HomePanel.Visible = false;
+
+            //產生icon buttons
+            for (int i = 0, j = 0, x = 40, y = 30; i < 15; i++, j++, x += 175)
+            {
+                if (j == 5)
+                {
+                    x = 40;
+                    y += 130;
+                    j = 0;
+                }
+                iconBtns[i] = new Button();
+                iconBtns[i].SetBounds(x, y, 170, 80);
+                iconBtns[i].ImageList = button_icons;
+                iconBtns[i].ImageIndex = i;
+                addPanel.Controls.Add(iconBtns[i]);
+                //iconBtns[i].MouseLeave += iconBtn_mouseLeave;
+                //iconBtns[i].MouseHover += iconBtn_mouseHover;
+
+                //button 外觀
+                iconBtns[i].FlatStyle = FlatStyle.Flat;
+                iconBtns[i].FlatAppearance.BorderSize = 2;
+                iconBtns[i].FlatAppearance.BorderColor = Color.White;
+                iconBtns[i].FlatAppearance.MouseOverBackColor = Color.BurlyWood;
+                iconBtns[i].ForeColor = Color.DarkBlue;
+                iconBtns[i].ImageAlign = ContentAlignment.TopCenter;
+                iconBtns[i].TextAlign = ContentAlignment.BottomCenter;
+                iconBtns[i].Font = new Font("Ink Free", 15, FontStyle.Bold);
+                switch (i)
+                {
+                    case 0: iconBtns[i].Text = "Food"; break;
+                    case 1: iconBtns[i].Text = "Shopping"; break;
+                    case 2: iconBtns[i].Text = "Transportation"; break;
+                    case 3: iconBtns[i].Text = "Entertainment"; break;
+                    case 4: iconBtns[i].Text = "Education"; break;
+                    case 5: iconBtns[i].Text = "Bills"; break;
+                    case 6: iconBtns[i].Text = "Social"; break;
+                    case 7: iconBtns[i].Text = "Sport"; break;
+                    case 8: iconBtns[i].Text = "Medical"; break;
+                    case 9: iconBtns[i].Text = "Taxes"; break;
+                    case 10: iconBtns[i].Text = "Investments"; break;
+                    case 11: iconBtns[i].Text = "Donation"; break;
+                    case 12: iconBtns[i].Text = "Insurance"; break;
+                    case 13: iconBtns[i].Text = "Travel"; break;
+                    case 14: iconBtns[i].Text = "Others"; break;
+                }
+                //cardBtns[i].Click += ButtonArray_Click;  //匿名函數 匿名函數中含有別的具名函數
+            }
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moneyBook_Load(object sender, EventArgs e)
+        {
+            iconBtns = new Button[15];
+            addPanel.Visible = false;
+            HomePanel.Visible = true;
+        }
+
+        private void iconBtn_mouseHover(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackColor = Color.BurlyWood;
+        }
+
+        private void iconBtn_mouseLeave(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackColor = Color.AntiqueWhite;
+        }
+
+        private void addBackBtn_Click(object sender, EventArgs e)
+        {
+            addPanel.Visible = false;
+            HomePanel.Visible = true;
         }
     }
 }
